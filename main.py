07ac3f7eb9286ac08127c05206cda586f7ff43aa
BakirovDanil <danil.bakiroff84@gmail.com>
2023-11-22 20:25:14 +0300

    Расположение объектов

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 import object
 
 Frame = Tk()
-# Значения для первой матрицы
+# Значения для первой матрицы (по критериям)
 matrix_1_1 = IntVar()
 matrix_1_1.set(1)
 matrix_1_2 = IntVar()
@@ -21,6 +21,57 @@ matrix_0 = [[matrix_1_1, matrix_1_2, matrix_1_3],
             [matrix_2_1, matrix_2_2, matrix_2_3],
             [matrix_3_1, matrix_3_2, matrix_3_3]]
 
+# значения для второй матрицы (по альтернативам и первому критерию)
+matrix1_1_1 = IntVar()
+matrix1_1_1.set(1)
+matrix1_1_2 = IntVar()
+matrix1_1_3 = IntVar()
+matrix1_2_1 = IntVar()
+matrix1_2_2 = IntVar()
+matrix1_2_2.set(1)
+matrix1_2_3 = IntVar()
+matrix1_3_1 = IntVar()
+matrix1_3_2 = IntVar()
+matrix1_3_3 = IntVar()
+matrix1_3_3.set(1)
+matrix_1 = [[matrix1_1_1, matrix1_1_2, matrix1_1_3],
+            [matrix1_2_1, matrix1_2_2, matrix1_2_3],
+            [matrix1_3_1, matrix1_3_2, matrix1_3_3]]
+
+# значения для третьей матрицы (по альтернативам и второму критерию)
+matrix2_1_1 = IntVar()
+matrix2_1_1.set(1)
+matrix2_1_2 = IntVar()
+matrix2_1_3 = IntVar()
+matrix2_2_1 = IntVar()
+matrix2_2_2 = IntVar()
+matrix2_2_2.set(1)
+matrix2_2_3 = IntVar()
+matrix2_3_1 = IntVar()
+matrix2_3_2 = IntVar()
+matrix2_3_3 = IntVar()
+matrix2_3_3.set(1)
+matrix_2 = [[matrix2_1_1, matrix2_1_2, matrix2_1_3],
+            [matrix2_2_1, matrix2_2_2, matrix2_2_3],
+            [matrix2_3_1, matrix2_3_2, matrix2_3_3]]
+
+# значения для четвертой матрицы (по альтернативам и третьему критерию)
+matrix3_1_1 = IntVar()
+matrix3_1_1.set(1)
+matrix3_1_2 = IntVar()
+matrix3_1_3 = IntVar()
+matrix3_2_1 = IntVar()
+matrix3_2_2 = IntVar()
+matrix3_2_2.set(1)
+matrix3_2_3 = IntVar()
+matrix3_3_1 = IntVar()
+matrix3_3_2 = IntVar()
+matrix3_3_3 = IntVar()
+matrix3_3_3.set(1)
+matrix_3 = [[matrix3_1_1, matrix3_1_2, matrix3_1_3],
+            [matrix3_2_1, matrix3_2_2, matrix3_2_3],
+            [matrix3_3_1, matrix3_3_2, matrix3_3_3]]
+
 
 def Maths():
     object.Raschet(matrix_0)
@@ -33,14 +84,31 @@ def Finish():
 
 def MainForm(window):
     window.title("Бакиров Данил, Валеев Марат, Баембитов Гата, Богомолов Валентин. Метод анализа иерархий")
-    window.geometry("800x600+400+200")
+    window.geometry("800x750+400+200")
     window.resizable(False, False)
     window.protocol("WM_DELETE_WINDOW", Finish)
     label = object.Label()
     label.Sozdanie(window)
-    Entry1 = object.Matrix0(matrix_1_1, matrix_1_2, matrix_1_3, matrix_2_1, matrix_2_2, matrix_2_3,
+    #
+    Entry1 = object.Matrix0(matrix_1_1, matrix_1_2, matrix_1_3,
+                            matrix_2_1, matrix_2_2, matrix_2_3,
                             matrix_3_1, matrix_3_2, matrix_3_3)
     Entry1.Sozdanie(window)
+    #
+    Entry2 = object.Matrix1(matrix1_1_1, matrix1_1_2, matrix1_1_3,
+                            matrix1_2_1, matrix1_2_2, matrix1_2_3,
+                            matrix1_3_1, matrix1_3_2, matrix1_3_3)
+    Entry2.Sozdanie(window)
+    #
+    Entry3 = object.Matrix2(matrix2_1_1, matrix2_1_2, matrix2_1_3,
+                            matrix2_2_1, matrix2_2_2, matrix2_2_3,
+                            matrix2_3_1, matrix2_3_2, matrix2_3_3)
+    Entry3.Sozdanie(window)
+    #
+    Entry4 = object.Matrix3(matrix3_1_1, matrix3_1_2, matrix3_1_3,
+                            matrix3_2_1, matrix3_2_2, matrix3_2_3,
+                            matrix3_3_1, matrix3_3_2, matrix3_3_3)
+    Entry4.Sozdanie(window)
     window.mainloop()
 
 
--- object.py
+++ object.py
@@ -13,7 +13,7 @@ def proverka(m):
     found_invalid_value = False
     for i in range(len(m)):
         for j in range(len(m[i])):
-            if i != j and m[i][j].get() > 9:
+            if m[i][j].get() > 9 or m[i][j].get() == 0:
                 found_invalid_value = True
                 m[i][j].set(0)
 
@@ -43,23 +43,76 @@ class Figure(ABC):
 
 class Label(Figure):
     def Sozdanie(self, window):
+        # Размещение текстовых полей критерий по вертикали
         crit1h = ttk.Label(text="Критерий 1")
         crit2h = ttk.Label(text="Критерий 2")
         crit3h = ttk.Label(text="Критерий 3")
+        # Размещение текстовых полей критерий по горизонтали
         crit1v = ttk.Label(text="Критерий 1")
         crit2v = ttk.Label(text="Критерий 2")
         crit3v = ttk.Label(text="Критерий 3")
+        # Размещение текстовых полей критерий по вертикали
+        alt11h = ttk.Label(text="Альтернатива 1")
+        alt12h = ttk.Label(text="Альтернатива 2")
+        alt13h = ttk.Label(text="Альтернатива 3")
+        # Размещение текстовых полей критерий по горизонтали
+        alt11v = ttk.Label(text="Альтер. 1")
+        alt12v = ttk.Label(text="Альтер. 2")
+        alt13v = ttk.Label(text="Альтер. 3")
+        # Размещение текстовых полей критерий по вертикали
+        alt21h = ttk.Label(text="Альтернатива 1")
+        alt22h = ttk.Label(text="Альтернатива 2")
+        alt23h = ttk.Label(text="Альтернатива 3")
+        # Размещение текстовых полей критерий по горизонтали
+        alt21v = ttk.Label(text="Альтер. 1")
+        alt22v = ttk.Label(text="Альтер. 2")
+        alt23v = ttk.Label(text="Альтер. 3")
+        # Размещение текстовых полей критерий по вертикали
+        alt31h = ttk.Label(text="Альтернатива 1")
+        alt32h = ttk.Label(text="Альтернатива 2")
+        alt33h = ttk.Label(text="Альтернатива 3")
+        # Размещение текстовых полей критерий по горизонтали
+        alt31v = ttk.Label(text="Альтер. 1")
+        alt32v = ttk.Label(text="Альтер. 2")
+        alt33v = ttk.Label(text="Альтер. 3")
+        #
         crit1h.place(x=25, y=40)
         crit2h.place(x=25, y=85)
         crit3h.place(x=25, y=130)
+        #
         crit1v.place(x=110, y=20)
         crit2v.place(x=190, y=20)
         crit3v.place(x=270, y=20)
+        #
+        alt11h.place(x=20, y=180)
+        alt12h.place(x=20, y=225)
+        alt13h.place(x=20, y=270)
+        #
+        alt11v.place(x=110, y=160)
+        alt12v.place(x=190, y=160)
+        alt13v.place(x=270, y=160)
+        #
+        alt21h.place(x=20, y=320)
+        alt22h.place(x=20, y=365)
+        alt23h.place(x=20, y=410)
+        #
+        alt21v.place(x=110, y=440)
+        alt22v.place(x=190, y=440)
+        alt23v.place(x=270, y=440)
+        #
+        alt31h.place(x=20, y=460)
+        alt32h.place(x=20, y=505)
+        alt33h.place(x=20, y=550)
+        #
+        alt31v.place(x=110, y=300)
+        alt32v.place(x=190, y=300)
+        alt33v.place(x=270, y=300)
 
 
 class Matrix0(Figure):
-    def __init__(self, matrix_1_1, matrix_1_2, matrix_1_3, matrix_2_1, matrix_2_2, matrix_2_3, matrix_3_1, matrix_3_2,
-                 matrix_3_3):
+    def __init__(self, matrix_1_1, matrix_1_2, matrix_1_3,
+                 matrix_2_1, matrix_2_2, matrix_2_3,
+                 matrix_3_1, matrix_3_2, matrix_3_3):
         self.matrix_1_1 = matrix_1_1
         self.matrix_1_2 = matrix_1_2
         self.matrix_1_3 = matrix_1_3
@@ -92,3 +145,117 @@ class Matrix0(Figure):
         m31.place(x=110, y=130)
         m32.place(x=190, y=130)
         m33.place(x=270, y=130)
+
+
+class Matrix1(Figure):
+    def __init__(self, matrix1_1_1, matrix1_1_2, matrix1_1_3,
+                 matrix1_2_1, matrix1_2_2, matrix1_2_3,
+                 matrix1_3_1, matrix1_3_2, matrix1_3_3):
+        self.matrix1_1_1 = matrix1_1_1
+        self.matrix1_1_2 = matrix1_1_2
+        self.matrix1_1_3 = matrix1_1_3
+        self.matrix1_2_1 = matrix1_2_1
+        self.matrix1_2_2 = matrix1_2_2
+        self.matrix1_2_3 = matrix1_2_3
+        self.matrix1_3_1 = matrix1_3_1
+        self.matrix1_3_2 = matrix1_3_2
+        self.matrix1_3_3 = matrix1_3_3
+
+    def Sozdanie(self, window):
+        m111 = ttk.Entry(width=5, textvariable=self.matrix1_1_1, state="readonly")
+        m112 = ttk.Entry(width=5, textvariable=self.matrix1_1_2)
+        m113 = ttk.Entry(width=5, textvariable=self.matrix1_1_3)
+        m121 = ttk.Entry(width=5, textvariable=self.matrix1_2_1, state="readonly")
+        m122 = ttk.Entry(width=5, textvariable=self.matrix1_2_2, state="readonly")
+        m123 = ttk.Entry(width=5, textvariable=self.matrix1_2_3)
+        m131 = ttk.Entry(width=5, textvariable=self.matrix1_3_1, state="readonly")
+        m132 = ttk.Entry(width=5, textvariable=self.matrix1_3_2, state="readonly")
+        m133 = ttk.Entry(width=5, textvariable=self.matrix1_3_3, state="readonly")
+        matrix0 = [m111, m112, m113, m121, m122, m123, m131, m132, m133]
+        for i in matrix0:
+            i.bind("<Key>", on_key_press)
+        m111.place(x=110, y=180)
+        m112.place(x=190, y=180)
+        m113.place(x=270, y=180)
+        m121.place(x=110, y=225)
+        m122.place(x=190, y=225)
+        m123.place(x=270, y=225)
+        m131.place(x=110, y=270)
+        m132.place(x=190, y=270)
+        m133.place(x=270, y=270)
+
+
+class Matrix2(Figure):
+    def __init__(self, matrix2_1_1, matrix2_1_2, matrix2_1_3,
+                 matrix2_2_1, matrix2_2_2, matrix2_2_3,
+                 matrix2_3_1, matrix2_3_2, matrix2_3_3):
+        self.matrix2_1_1 = matrix2_1_1
+        self.matrix2_1_2 = matrix2_1_2
+        self.matrix2_1_3 = matrix2_1_3
+        self.matrix2_2_1 = matrix2_2_1
+        self.matrix2_2_2 = matrix2_2_2
+        self.matrix2_2_3 = matrix2_2_3
+        self.matrix2_3_1 = matrix2_3_1
+        self.matrix2_3_2 = matrix2_3_2
+        self.matrix2_3_3 = matrix2_3_3
+
+    def Sozdanie(self, window):
+        m211 = ttk.Entry(width=5, textvariable=self.matrix2_1_1, state="readonly")
+        m212 = ttk.Entry(width=5, textvariable=self.matrix2_1_2)
+        m213 = ttk.Entry(width=5, textvariable=self.matrix2_1_3)
+        m221 = ttk.Entry(width=5, textvariable=self.matrix2_2_1, state="readonly")
+        m222 = ttk.Entry(width=5, textvariable=self.matrix2_2_2, state="readonly")
+        m223 = ttk.Entry(width=5, textvariable=self.matrix2_2_3)
+        m231 = ttk.Entry(width=5, textvariable=self.matrix2_3_1, state="readonly")
+        m232 = ttk.Entry(width=5, textvariable=self.matrix2_3_2, state="readonly")
+        m233 = ttk.Entry(width=5, textvariable=self.matrix2_3_3, state="readonly")
+        matrix0 = [m211, m212, m213, m221, m222, m223, m231, m232, m233]
+        for i in matrix0:
+            i.bind("<Key>", on_key_press)
+        m211.place(x=110, y=320)
+        m212.place(x=190, y=320)
+        m213.place(x=270, y=320)
+        m221.place(x=110, y=365)
+        m222.place(x=190, y=365)
+        m223.place(x=270, y=365)
+        m231.place(x=110, y=410)
+        m232.place(x=190, y=410)
+        m233.place(x=270, y=410)
+
+
+class Matrix3(Figure):
+    def __init__(self, matrix3_1_1, matrix3_1_2, matrix3_1_3,
+                 matrix3_2_1, matrix3_2_2, matrix3_2_3,
+                 matrix3_3_1, matrix3_3_2, matrix3_3_3):
+        self.matrix3_1_1 = matrix3_1_1
+        self.matrix3_1_2 = matrix3_1_2
+        self.matrix3_1_3 = matrix3_1_3
+        self.matrix3_2_1 = matrix3_2_1
+        self.matrix3_2_2 = matrix3_2_2
+        self.matrix3_2_3 = matrix3_2_3
+        self.matrix3_3_1 = matrix3_3_1
+        self.matrix3_3_2 = matrix3_3_2
+        self.matrix3_3_3 = matrix3_3_3
+
+    def Sozdanie(self, window):
+        m311 = ttk.Entry(width=5, textvariable=self.matrix3_1_1, state="readonly")
+        m312 = ttk.Entry(width=5, textvariable=self.matrix3_1_2)
+        m313 = ttk.Entry(width=5, textvariable=self.matrix3_1_3)
+        m321 = ttk.Entry(width=5, textvariable=self.matrix3_2_1, state="readonly")
+        m322 = ttk.Entry(width=5, textvariable=self.matrix3_2_2, state="readonly")
+        m323 = ttk.Entry(width=5, textvariable=self.matrix3_2_3)
+        m331 = ttk.Entry(width=5, textvariable=self.matrix3_3_1, state="readonly")
+        m332 = ttk.Entry(width=5, textvariable=self.matrix3_3_2, state="readonly")
+        m333 = ttk.Entry(width=5, textvariable=self.matrix3_3_3, state="readonly")
+        matrix0 = [m311, m312, m313, m321, m322, m323, m331, m332, m333]
+        for i in matrix0:
+            i.bind("<Key>", on_key_press)
+        m311.place(x=110, y=460)
+        m312.place(x=190, y=460)
+        m313.place(x=270, y=460)
+        m321.place(x=110, y=505)
+        m322.place(x=190, y=505)
+        m323.place(x=270, y=505)
+        m331.place(x=110, y=550)
+        m332.place(x=190, y=550)
+        m333.place(x=270, y=550)
